680c81fb775bff042c22dea32356843b2f70ad75
lol-Z7 <98399273+lol-Z7@users.noreply.github.com>
2024-11-01 16:54:10 +0300

    Add files via upload

---
+++ A program that calculates the graduation percentage in high school second year.py
@@ -0,0 +1,17 @@
+print("Welcome to the program according to academic percentage")
+physics = float(input("Enter your degree in Physics:"))
+chemistry = float(input("Enter your degree in chemistry:"))
+math = float(input("Enter your degree in math:"))
+English = float(input("Enter your degere in Einglish:"))
+Arabic = float(input("Enter your degree in Arabic:"))
+Biolgy = float(input("Enter your degree in Biolgy:"))
+digital_technology = float(input("Enter your degree in digital technology:"))
+physics1 = physics * 0.1667
+chemistry1 = chemistry * 0.1667
+math1 = math * 0.1667
+English1 = English * 0.1667
+Biolgy1 = Biolgy * 0.1333
+Arabic1 = Arabic * 0.1
+digital_technology1 = digital_technology * 0.1
+
+print(English1 + math1 + chemistry1 + physics1 + digital_technology1 + Biolgy1 + Arabic1 - 0.00999999999999 )
